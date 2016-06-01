46e1b30e8a0af7af476e1b63ece14aace0bedd83
funnytao <funnytao@bu.edu>
2016-06-01 15:20:49 -0400

    Update sw.js

--- caltrain/sw.js
+++ caltrain/sw.js
@@ -12,15 +12,15 @@ this.addEventListener('install', function(event) {
   );
 });
 
-// this.addEventListener('fetch', function(event) {
-//   var response;
-//   event.respondWith(caches.match(event.request).catch(function() {
-//     return fetch(event.request);
-//   }).then(function(r) {
-//     response = r;
-//     caches.open('v1').then(function(cache) {
-//       cache.put(event.request, response);
-//     });
-//     return response.clone();
-//   }));
-// });
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }));
+});
