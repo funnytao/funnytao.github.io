076731983f5af28896c93efb0e33c5c67bedc384
funnytao <funnytao@bu.edu>
2016-06-02 10:06:24 -0400

    update

--- caltrain/sw.js
+++ caltrain/sw.js
@@ -5,22 +5,32 @@ this.addEventListener('install', function(event) {
         '/caltrain/',
         '/caltrain/index.html',
         '/caltrain/css/style.css',
-        '/caltrain/js/all.js',
-        '/caltrain/css/bootstrap.min.css'
+        '/caltrain/js/all.min.js',
+        '/caltrain/js/googlemap.js',
+        '/caltrain/css/bootstrap.min.css',
+        '/caltrain/logo.png',
+        'https://funnytao.github.io/caltrain/caltrain/stops.json',
+        'https://funnytao.github.io/caltrain/caltrain/stop_time.json',
+        'https://funnytao.github.io/caltrain/caltrain/trips.json',
+        'https://funnytao.github.io/caltrain/caltrain/stop_list.json'
       ]);
     })
   );
 });
 
  this.addEventListener('fetch', function(event) {
-   var response;
-   event.respondWith(caches.match(event.request).catch(function() {
-     return fetch(event.request);
-   }).then(function(r) {
-     response = r;
-     caches.open('v1').then(function(cache) {
-       cache.put(event.request, response);
-     });
-     return response.clone();
-   }));
- });
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/caltrain'));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
