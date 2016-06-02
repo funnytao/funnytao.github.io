62a70bee23d4c53f7cef2ce2972edb786361e512
funnytao <funnytao@bu.edu>
2016-06-02 10:08:47 -0400

    update

--- caltrain/sw.js
+++ caltrain/sw.js
@@ -2,19 +2,13 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/caltrain/',
-        '/caltrain/index.html',
-        '/caltrain/css/style.css',
-<<<<<<< HEAD
-        '/caltrain/js/all.min.js',
-        '/caltrain/js/googlemap.js',
-        '/caltrain/css/bootstrap.min.css',
-        '/caltrain/logo.png',
-=======
-        '/caltrain/js/all.js',
-        '/caltrain/js/googlemap.js',
-        '/caltrain/css/bootstrap.min.css',
->>>>>>> origin/master
+        '/',
+        '/index.html',
+        '/css/style.css',
+        '/js/all.min.js',
+        '/js/googlemap.js',
+        '/css/bootstrap.min.css',
+        '/logo.png',
         'https://funnytao.github.io/caltrain/caltrain/stops.json',
         'https://funnytao.github.io/caltrain/caltrain/stop_time.json',
         'https://funnytao.github.io/caltrain/caltrain/trips.json',
@@ -24,32 +18,19 @@ this.addEventListener('install', function(event) {
   );
 });
 
-// this.addEventListener('fetch', function(event) {
-//   var response;
-//   event.respondWith(caches.match(event.request).catch(function() {
-//     return fetch(event.request);
-//   }).then(function(r) {
-//     response = r;
-//     caches.open('v1').then(function(cache) {
-//       cache.put(event.request, response);
-//     });
-//     return response.clone();
-//   }));
-// });
+this.addEventListener('fetch', function(event) {
+ var requestUrl = new URL(event.request.url);
 
- this.addEventListener('fetch', function(event) {
-  var requestUrl = new URL(event.request.url);
+ if (requestUrl.origin === location.origin) {
+   if (requestUrl.pathname === '/') {
+     event.respondWith(caches.match('/'));
+     return;
+   }
+ }
 
-  if (requestUrl.origin === location.origin) {
-    if (requestUrl.pathname === '/') {
-      event.respondWith(caches.match('/caltrain'));
-      return;
-    }
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
 });
